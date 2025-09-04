e81ae70688b72a3876a4dfbc60762848c3515aa6
dev-tsampaio <thalessampaiovp@icloud.com>
2025-09-04 11:49:18 -0300

    docs(projeto): Projeto criado!

---
+++ src/main/java/com/github/dev_tsampaio/projetowebservices/ProjetowebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.github.dev_tsampaio.projetowebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebservicesApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(ProjetowebservicesApplication.class, args);
+	}
+
+}

d4686ced9c12793d43dff54d99114f835362a12f
dev-tsampaio <thalessampaiovp@icloud.com>
2025-09-05 09:25:07 -0300

    perf(Category): Importei o lombok para ter um codigo mais cleanna classe Category

--- src/main/java/com/github/dev_tsampaio/projetowebservices/entities/Category.java
+++ src/main/java/com/github/dev_tsampaio/projetowebservices/entities/Category.java
@@ -2,6 +2,7 @@ package com.github.dev_tsampaio.projetowebservices.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -10,6 +11,11 @@ import java.util.Set;
 
 @Entity
 @Table(name ="tb_category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -23,46 +29,10 @@ public class Category implements Serializable {
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
-    public Category() {}
-
     public Category(Long id, String name) {
         this.id = id;
         this.name = name;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Product> getProducts() {
-        return products;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Category category)) return false;
-        return Objects.equals(id, category.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-
 
 }
 

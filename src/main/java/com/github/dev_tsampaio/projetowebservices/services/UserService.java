b41094a6237a7a9f3ff168cc620a5d5c2188dd4f
dev-tsampaio <thalessampaiovp@icloud.com>
2025-09-07 10:46:56 -0300

    feat(Update): Tratamento de excessões da função update

--- src/main/java/com/github/dev_tsampaio/projetowebservices/services/UserService.java
+++ src/main/java/com/github/dev_tsampaio/projetowebservices/services/UserService.java
@@ -5,6 +5,7 @@ import com.github.dev_tsampaio.projetowebservices.entities.User;
 import com.github.dev_tsampaio.projetowebservices.repositories.UserRepository;
 import com.github.dev_tsampaio.projetowebservices.services.exceptions.DataBaseException;
 import com.github.dev_tsampaio.projetowebservices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
